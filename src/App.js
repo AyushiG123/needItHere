e2d1d71e3163d74487c83be5ba290e3826d1e797
Ayushi Gupta <ayushi.pixie@gmail.com>
2018-07-31 15:55:03 -0400

    Styled and laid out headings

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={styles}>
       <h1>
-        I Need It Here!!
+        #NEED
       </h1>
+      <h2> 
+        Where the needs of the community make a stand
+        </h2>
       </div>
     );
+  }
+}
+
+const styles={
+  h1:{
+    color: "blue",
+    fontSize:"80px",
   }
 }
 
